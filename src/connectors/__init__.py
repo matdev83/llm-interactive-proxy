256bd64a2f1ca0849295805a963df9abea90be41
matdev83 <github@matdev83.anonaddy.com>
2025-09-27 23:24:43 +0200

    2150 passed in 37.02s

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -2,6 +2,8 @@ from .anthropic import AnthropicBackend
 from .anthropic_oauth import AnthropicOAuthBackend
 from .base import LLMBackend
 from .gemini import GeminiBackend
+from .gemini_cloud_project import GeminiCloudProjectConnector
+from .gemini_oauth_personal import GeminiOAuthPersonalConnector
 from .openai import OpenAIConnector
 from .openai_oauth import OpenAIOAuthConnector
 from .openrouter import OpenRouterBackend
@@ -13,6 +15,8 @@ __all__ = [
     "AnthropicBackend",
     "AnthropicOAuthBackend",
     "GeminiBackend",
+    "GeminiCloudProjectConnector",
+    "GeminiOAuthPersonalConnector",
     "LLMBackend",
     "OpenAIConnector",
     "OpenAIOAuthConnector",
